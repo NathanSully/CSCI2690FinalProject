985fe3cff3a96278c5d4126cf55655ef0befc440
ngotu <tn610084@dal.ca>
2025-03-18 14:49:06 -0300

    added

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const hostname = "localhost";
 const port = 8000;
 
+const html = fs.readFileSync(`${__dirname}/index.html`, "utf-8");
+const tutorHtml = fs.readFileSync(`${__dirname}/tutor.html`, "utf-8");
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/courses.json`, "utf-8"));
+
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.end("<h1>Hello from the server-side!</h1>");
+    if (request.url === "/courses") {
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(courses));
+    } else if (request.url === "/tutors") {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(tutorHtml);
+    } else {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(html);
+    }
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
