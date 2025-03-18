e1f7700ced0a2c70ac2e642ecdfd8c69575f4159
Nathan Sullivan <nt384140@dal.ca>
2025-03-18 14:45:02 -0300

    Added routing to server.js

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
+// Imports
 const http = require("http");
+const url = require('url');
+const fs = require('fs');
 
+// Parameters
 const hostname = "localhost";
 const port = 8000;
 
+// Loading html from files
+const homepageHTML = fs.readFileSync("./public/index.html", "utf-8");
+
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.end("<h1>Hello from the server-side!</h1>");
+    // Extracting pathname
+    const { pathname } = url.parse(request.url, true);
+
+    // Routing
+    if (pathname === "/home" || pathname === "/") {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.end(homepageHTML);
+    }
+    else if (pathname === "/about") {
+        response.end("About");
+    }
+    else {
+        response.writeHead(404, {"Content-Type": "text/html"});
+        response.end("Page not found");
+    }
 });
 
+// Listening on the port
 server.listen(port, hostname, () => {
     console.log(`Listening on port ${port}`);
 });
+
