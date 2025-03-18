5255408dd753fe08519056d84dab170eddb93898
Nathan Sullivan <nt384140@dal.ca>
2025-03-18 15:37:27 -0300

    Fixed static images being loaded

--- server.js
+++ server.js
@@ -1,17 +1,49 @@
+// Imports
 const http = require("http");
 const fs = require("fs");
 const url = require('url');
 
+// Parameters
 const hostname = "localhost";
 const port = 8000;
 
+// Loading HTML
 const homepageHTML = fs.readFileSync(`${__dirname}/public/index.html`, "utf-8");
 const headerHTML = fs.readFileSync(`${__dirname}/templates/header.html`, "utf-8");
 const tutorHtml = fs.readFileSync(`${__dirname}/public/tutor.html`, "utf-8");
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/files/courses.json`, "utf-8"));
 const searchHtml =  fs.readFileSync(`${__dirname}/templates/search.html`, "utf-8");
+
+// Creating the server
 const server = http.createServer((request, response) => {
     const { pathname } = url.parse(request.url, true);
+
+    // Serving static files
+    if (pathname.startsWith('/files/')) {
+        const filePath = `${__dirname}/${pathname}`;
+        fs.readFile(filePath, (error, data) => {
+            if (error) {
+                response.writeHead(404, { "Content-Type": "text/html" });
+                response.end("File Not Found!");
+            } else {
+                // Finding and setting the content type based on the file extension
+                const extension = filePath.split(".")[-1];
+                let contentType = "text/plain";
+                if (extension === ".jpeg" || extension === ".jpg") {
+                    contentType = "image/jpeg";
+                } else if (extension === ".css") {
+                    contentType = "text/css";
+                } else if (extension === ".js") {
+                    contentType = "application/javascript";
+                }
+
+                // Responding with the correct content type
+                response.writeHead(200, { "Content-Type": contentType });
+                response.end(data);
+            }
+        });
+        return;
+    }
 
     // Routing
     if (pathname === "/home" || pathname === "/") {
